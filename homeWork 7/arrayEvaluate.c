4d38b429e4ce2bbd749abbe2a1ea9eb82647cf29
dando <95683796+LyRiCaLLL@users.noreply.github.com>
2022-03-25 09:59:19 +0200

    Update arrayEvaluate.c

--- homeWork 7/arrayEvaluate.c
+++ homeWork 7/arrayEvaluate.c
@@ -1,18 +1,18 @@
 #include <stdio.h>
 
 int sum(int *arr, size_t size );
-void arrayEvaluate(int* arr, size_t n, int(*f)(int*,int));
+void arrayEvaluate(int* arr, size_t n, int(*f)(int*,size_t));
 
 int main(){
     size_t n = 5;
     int arr []= {1,2,3,4,5};
 
-   int(*f)(int*, int);
+   int(*f)(int*, size_t);
     
     arrayEvaluate(arr, n, sum);
     return 0;
 }
-void arrayEvaluate(int* arr, size_t n, int(*f)(int*,int)) {
+void arrayEvaluate(int* arr, size_t n, int(*f)(int*,size_t)) {
     printf("%d", f(arr, n));
 }
 int sum(int *arr, size_t size ){
